7744d388bbb18b2e2806d699121bb8badcae6ed1
Avagyan02 <samvel.avagyan.02@bk.ru>
2023-01-04 23:00:36 +0400

    chore(Core): add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        // type: config.get<string>('TYPEORM_CONNECTION'),
+        // username: config.get<string>('TYPEORM_USERNAME'),
+        // password: config.get<string>('TYPEORM_PASSWORD'),
+        // database: config.get<string>('TYPEORM_DATABASE'),
+        // port: config.get<number>('TYPEORM_PORT'),
+        // entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        // synchronize: true,
+        // autoLoadEntities: true,
+        // logging: true,
+
+        type: 'postgres',
+        username: 'postgres',
+        password: 'root',
+        database: 'social_network',
+        port: 5432,
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
